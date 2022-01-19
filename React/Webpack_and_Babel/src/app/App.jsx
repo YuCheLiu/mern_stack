71567ce5a11d0f6f3efdfd0bdb8d351dc3a44ce4
YuChe Liu <watch776@yahoo.com.tw>
2022-01-18 16:37:26 -0800

    add React Jsx source code

---
+++ React/Webpack_and_Babel/src/app/App.jsx
@@ -0,0 +1,9 @@
+// import ReactDOM from "react";
+
+// function Welcome(){
+//     return <h1>Hello from App.jsx</h1>
+// }
+
+// const element = <Welcome></Welcome>;
+    
+// ReactDOM.render(element, document.getElementById('root'));
