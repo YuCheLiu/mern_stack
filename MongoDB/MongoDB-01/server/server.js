e9612834e5f0699e803d2bee1a01f6e5e5abe3ca
YuChe Liu <watch776@yahoo.com.tw>
2022-01-30 18:34:08 -0800

    update aggregate function example

--- MongoDB/MongoDB-01/server/server.js
+++ MongoDB/MongoDB-01/server/server.js
@@ -15,13 +15,39 @@ async function connect(){
   await client.connect();  
   console.log("Connected to CS628 database");
   const db = client.db();
-  find(db);
+  findAggreate(db);
   return db;
 }
 
 async function find(db){
-  const cursor = await db.collection('listingsAndReviews').find({price: {$gt :500}}).toArray();
-  cursor.forEach((list)=>{
+  const cursor = await db.collection('listingsAndReviews').findOne({price: {$gt :1000}}).toArray();
+  cursor.map((list)=>{
     console.log(list["price"]);
   })
+}
+
+async function findOne(db){
+  const cursor = await db.collection('listingsAndReviews').findOne({price: {$gt :1000}});
+ 
+  console.log(cursor);
+}
+
+async function findAggreate(db){
+  const pipeline = [
+    {
+      '$match': {
+        'price': 1000
+      }
+    },
+    {
+      '$project': {
+        'name': 1,
+        'price':1,
+        'accommodates':1
+      }
+    }
+
+  ]
+  const cursor = await db.collection('listingsAndReviews').aggregate(pipeline).toArray();
+  cursor.map( (list) => {console.log(list)})
 }
