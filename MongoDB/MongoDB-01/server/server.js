4c50dd9b87c617264c1aa0351942822fd77f5a13
YuChe Liu <watch776@yahoo.com.tw>
2022-02-14 17:52:15 -0800

    add find method for MongoDB

--- MongoDB/MongoDB-01/server/server.js
+++ MongoDB/MongoDB-01/server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config()
 connect();
+
 const app = express();
 
 app.use(express.static('public'));
@@ -9,21 +10,59 @@ app.listen(3000, function () {
   console.log('App started on port 3000');
 });
 
+// async function test(){
+//   await console.log("Hello this is me");
+// }
+// var promise = new Promise(function(resolve, reject) {
+//   const x = "12";
+//   const y = "13"
+//   if(x === y) {
+//     resolve();
+//   } else {
+//     reject();
+//   }
+// });
+// promise.
+//     then(function () {
+//         console.log('1. Success, 12 = 12');
+//     }).
+//     catch(function () {
+//         console.log('2. Some error has occurred');
+//     });
+// console.log("3. Some result shows here.");
+
+// async function test(){
+//     console.log("1. inside test function")
+//     return "Hello World";
+// }
+
+// test().
+//   then(result =>{
+//     console.log("2. "+result);
+//   })
+//   .catch(e => {
+//     console.log("3. error");
+//   });
+
+
 async function connect(){
   const {MongoClient} = require('mongodb');
   const client = new MongoClient('mongodb+srv://'+process.env.MONGODB_ACCOUNT+':'+process.env.MONGODB_PASSWORD+'@cluster0.swxaj.mongodb.net/sample_airbnb');
-  await client.connect();  
+  await client.connect();
   console.log("Connected to CS628 database");
   const db = client.db();
-  findAggreate(db);
+  find(db)
   return db;
 }
 
 async function find(db){
-  const cursor = await db.collection('listingsAndReviews').findOne({price: {$gt :1000}}).toArray();
-  cursor.map((list)=>{
-    console.log(list["price"]);
-  })
+  const query = {
+    price: {$lt : 10}
+  }
+  const cursor = await db.collection('listingsAndReviews').find(query);
+  
+  let result = await cursor.map((list)=>list.name)
+  result.forEach(item => console.log(item));
 }
 
 async function findOne(db){
